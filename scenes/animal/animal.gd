c32011a0129fa8736441ab749ace277f7fd94e16
Ketan Patel <ktnp1204@gmail.com>
2024-05-17 21:55:54 +0200

    Added animal states with mouse interaction behaviour

--- scenes/animal/animal.gd
+++ scenes/animal/animal.gd
@@ -1,6 +1,12 @@
 extends RigidBody2D
 
 
+enum ANIMAL_STATE { READY, DRAG, RELEASE }
+
+var _state: ANIMAL_STATE = ANIMAL_STATE.READY
+@onready var label = $Label
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,7 +14,39 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	pass
+	update(delta)
+	label.text = "%s" % ANIMAL_STATE.keys()[_state]
+
+
+func set_new_state(new_state: ANIMAL_STATE) -> void:
+	_state = new_state
+	if _state == ANIMAL_STATE.RELEASE:
+		freeze = false
+	elif _state == ANIMAL_STATE.DRAG:
+		pass
+
+
+func detect_release() -> bool:
+	if _state == ANIMAL_STATE.DRAG:
+		if Input.is_action_just_released("drag"):
+			set_new_state(ANIMAL_STATE.RELEASE)
+			return true
+	return false
+
+
+func update_drag() -> void:
+	
+	if detect_release():
+		return
+	
+	var gmp = get_global_mouse_position()
+	position = gmp
+
+
+func update(delta: float) -> void:
+	match _state:
+		ANIMAL_STATE.DRAG: 
+			update_drag()
 
 
 func die() -> void:
@@ -18,3 +56,8 @@ func die() -> void:
 
 func _on_screen_exited():
 	die()
+
+
+func _on_input_event(viewport, event, shape_idx):
+	if _state == ANIMAL_STATE.READY and event.is_action_pressed("drag"):
+		set_new_state(ANIMAL_STATE.DRAG)
