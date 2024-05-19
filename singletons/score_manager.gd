33ff8780b22a3565289a502cd23a7813e29af882
Ketan Patel <ktnp1204@gmail.com>
2024-05-19 17:12:13 +0200

    Added saving scores to file

--- singletons/score_manager.gd
+++ singletons/score_manager.gd
@@ -1,10 +1,15 @@
 extends Node
 
+const DEFAULT_SCORE: int = 1000
+const SCORES_PATH = "user://animals.json"
+
+
 var _level_selected: int = 1
+var _level_scores: Dictionary = {} 
 
 
 func _ready():
-	pass
+	load_from_disk()
 
 
 func set_level_selected(ls: int) -> void:
@@ -13,3 +18,46 @@ func set_level_selected(ls: int) -> void:
 
 func get_level_selected() -> int:
 	return _level_selected
+
+
+func check_and_add(level: String) -> void:
+	if !_level_scores.has(level):
+		_level_scores[level] = DEFAULT_SCORE
+
+
+func set_score_for_level(score: int, level: String) -> void:
+	check_and_add(level)
+	if _level_scores[level] > score:
+		_level_scores[level] = score
+		save_to_disk()
+
+
+func get_best_for_level(level: String) -> int:
+	check_and_add(level)
+	return _level_scores[level]
+
+
+func save_to_disk() -> void:
+	var file = FileAccess.open(SCORES_PATH, FileAccess.WRITE)
+	var scores_json_str = JSON.stringify(_level_scores)
+	file.store_string(scores_json_str)
+
+
+func load_from_disk() -> void:
+	var file = FileAccess.open(SCORES_PATH, FileAccess.READ)
+	if file == null:
+		save_to_disk()
+	else:
+		var data = file.get_as_text()
+		_level_scores = JSON.parse_string(data)
+
+
+
+
+
+
+
+
+
+
+
